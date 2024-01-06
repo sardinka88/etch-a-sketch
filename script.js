8e2ea0690bd3b88ea0280b0ab9f1b866c5494dbd
Srdan <118986032+sardinka88@users.noreply.github.com>
2024-01-06 10:45:44 +0100

    Change handling of setActive
    Changed the condition so user clicks the canvas to toggle drawing
    mode. Allows for a better control.

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
+// define DOM elements
 const mainContainer = document.querySelector(".container");
 const gridContainer = document.querySelector(".grid");
 
+// define a function that creates a grid of X by X squares
 const generateGrid = (squareSize, numSquares) => {
   gridContainer.style.width = `${squareSize * numSquares}px`;
   for (let i = 0; i < numSquares; i++) {
     for (let j = 0; j < numSquares; j++) {
       const square = document.createElement("div");
       square.classList.add("square");
-      square.style.width = `${squareSize - 2}px`;
-      square.style.height = `${squareSize - 2}px`;
+      square.style.width = `${squareSize - 2}px`; //subtract border width
+      square.style.height = `${squareSize - 2}px`; //subtract border width
       gridContainer.appendChild(square);
     }
   }
@@ -21,19 +23,20 @@ const grid = document.querySelectorAll(".square");
 // Event Handling
 
 // track if mouse is pressed down
-let isMouseDown = false;
+let isMouseClicked = false;
 
-document.addEventListener("mousedown", () => {
-  isMouseDown = true;
+gridContainer.addEventListener("click", () => {
+  if (isMouseClicked === false) isMouseClicked = true;
+  else isMouseClicked = false;
 });
 
-document.addEventListener("mouseup", () => {
-  isMouseDown = false;
-});
+// document.addEventListener("mouseup", () => {
+//   isMouseDown = false;
+// });
 
 // if mouse is pressed down - apply the active class to a square
 const setActive = (event) => {
-  if (isMouseDown) event.target.classList.add("active");
+  if (isMouseClicked) event.target.classList.add("active");
 };
 
 // add mouseover event listener to all squares - sets the active class ONLY
