ce311b71eacd072dc002e567ea3a569574c2397b
Srdan <118986032+sardinka88@users.noreply.github.com>
2024-01-08 15:07:02 +0100

    Add grid size selection and dwg color selection
    This is still a bit messy, work in progress
    Need to style
    Need to refactor and clean up code

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
 // define DOM elements
 const mainContainer = document.querySelector(".container");
 const gridContainer = document.querySelector(".grid");
+const colorSelector = document.querySelector("#color");
+const sizeSelector = document.querySelector("#grid-size");
+const confirmSizeButton = document.querySelector(".select-grid-size");
+const selectColorButton = document.querySelector(".select-color");
+const resetButton = document.querySelector(".reset");
+let grid;
+let color = "";
+
+let gridSize = 30;
+const setGridSize = (size) => {
+  gridSize = size;
+  console.log(`Size set to ${size} x ${size}`);
+}
+
+const resetGrid = () => {
+  gridContainer.innerHTML = "";
+}
+
+const setColor = (newColor) => {
+  color = newColor;
+  console.log('Color set');
+}
+
 
 // define a function that creates a grid of X by X squares
-const generateGrid = (squareSize, numSquares) => {
+const generateGrid = (squareSize, numSquares = 30) => {
+  resetGrid();
   gridContainer.style.width = `${squareSize * numSquares}px`;
   for (let i = 0; i < numSquares; i++) {
     for (let j = 0; j < numSquares; j++) {
@@ -14,11 +38,24 @@ const generateGrid = (squareSize, numSquares) => {
       gridContainer.appendChild(square);
     }
   }
+  grid = document.querySelectorAll(".square");
+  console.log(`Generated grid`);
+  grid.forEach((square) => square.addEventListener("mouseover", setActive));
 };
 
-generateGrid(20, 30);
+confirmSizeButton.addEventListener('click', () => setGridSize(sizeSelector.value));
+confirmSizeButton.addEventListener('click', () => generateGrid(20, gridSize));
 
-const grid = document.querySelectorAll(".square");
+selectColorButton.addEventListener('click', ()=> setColor(colorSelector.value));
+
+resetButton.addEventListener("click", () =>{
+  gridSize = 0;
+  color = '';
+  colorSelector.value = "#000000";
+  sizeSelector.value = 0;
+  gridContainer.innerHTML = "";
+})
+
 
 // Event Handling
 
@@ -32,9 +69,12 @@ gridContainer.addEventListener("click", () => {
 
 // if mouse is pressed down - apply the active class to a square
 const setActive = (event) => {
-  if (isMouseClicked) event.target.classList.add("active");
+  if (isMouseClicked) {
+    event.target.classList.add("active");
+    event.target.style.backgroundColor = color;
+  }
 };
 
 // add mouseover event listener to all squares - sets the active class ONLY
 // if mouse is pressed down
-grid.forEach((square) => square.addEventListener("mouseover", setActive));
+// grid.forEach((square) => square.addEventListener("mouseover", setActive));
