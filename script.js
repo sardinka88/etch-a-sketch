0ca0678a87fd3e29ae818c98a4eeef68ae59bf9e
Srdan <118986032+sardinka88@users.noreply.github.com>
2024-01-10 17:35:40 +0100

    Add rainbow mode handling and toggle

--- script.js
+++ script.js
@@ -5,9 +5,13 @@ const colorSelector = document.querySelector("#color");
 const sizeSelector = document.querySelector("#grid-size");
 const confirmSizeButton = document.querySelector(".select-grid-size");
 const selectColorButton = document.querySelector(".select-color");
+const canvasColorSelector = document.querySelector("#canvas-color");
+const canvasColorButton = document.querySelector(".select-canvas-color");
 const resetButton = document.querySelector(".reset");
+const toggle = document.querySelector("#toggle");
 let grid;
 let color = "";
+let canvasColor = "";
 
 let gridSize = 30;
 const setGridSize = (size) => {
@@ -24,30 +28,52 @@ const setColor = (newColor) => {
   console.log('Color set');
 }
 
+const setCanvasColor = (newColor) =>{
+  canvasColor = newColor;
+}
+
+const getRandomColor = () => {
+  return randomColor = Math.floor(Math.random() *16777215).toString(16);
+
+}
+
 
 // define a function that creates a grid of X by X squares
-const generateGrid = (numSquares = 30) => {
-  const squareSize = 960 / numSquares;
-  resetGrid();
+const generateGrid = (numSquares) => {
+  const squareSize = 960 / numSquares; // calculate square size
+  resetGrid(); //clean up old content
   for (let i = 0; i < numSquares; i++) {
     for (let j = 0; j < numSquares; j++) {
       const square = document.createElement("div");
       square.classList.add("square");
       square.style.width = `${squareSize - 2}px`; //subtract border width
       square.style.height = `${squareSize - 2}px`; //subtract border width
+      square.style.backgroundColor = canvasColor;
       gridContainer.appendChild(square);
     }
   }
   grid = document.querySelectorAll(".square");
   console.log(`Generated grid`);
-  grid.forEach((square) => square.addEventListener("mouseover", setActive));
+  grid.forEach((square) => square.addEventListener("mouseover", setActive)); // set event listeners
 };
 
+
+// Event Handling
+
+// stores the square size and generates the grid accordingly
 confirmSizeButton.addEventListener('click', () => setGridSize(sizeSelector.value));
 confirmSizeButton.addEventListener('click', () => generateGrid(gridSize));
 
+// stores the selected drawing color
 selectColorButton.addEventListener('click', ()=> setColor(colorSelector.value));
 
+// stores the selected canvas color
+canvasColorButton.addEventListener("click", ()=> {
+  setCanvasColor(canvasColorSelector.value);
+
+});
+
+// resets the screen
 resetButton.addEventListener("click", () =>{
   gridSize = 0;
   color = '';
@@ -55,9 +81,6 @@ resetButton.addEventListener("click", () =>{
   sizeSelector.value = 0;
   gridContainer.innerHTML = "";
 })
-
-
-// Event Handling
 
 // track if mouse is clicked
 let isMouseClicked = false;
@@ -70,8 +93,12 @@ gridContainer.addEventListener("click", () => {
 // if mouse is pressed down - apply the active class to a square
 const setActive = (event) => {
   if (isMouseClicked) {
-    event.target.classList.add("active");
-    event.target.style.backgroundColor = color;
+    if(toggle.checked) {
+      event.target.style.backgroundColor = `#${getRandomColor()}`;
+    } else {
+      event.target.classList.add("active");
+      event.target.style.backgroundColor = color;
+    }
   }
 };
 
